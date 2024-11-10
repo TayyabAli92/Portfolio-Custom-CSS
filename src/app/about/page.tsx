af4b6473e541071306ea928e593f092550a7caa8
Tayyab Ali <hameshak110@gmail.com>
2024-11-11 00:26:41 +0500

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,20 @@
+import "../../style/about.css";
+
+export default function About() {
+  return (
+    <div className="about-box">
+      <div className="content-box">
+          <h1>About me</h1>
+        <p>
+          I'm a passionate and dedicated developer currently exploring the world
+          of modern web technologies, including Next.js, TypeScript. With a
+          strong foundation in front-end development, I strive to create
+          efficient and engaging web experiences. As I continue my journey, I
+          look forward to diving into Artificial Intelligence and advancing my
+          Python skills, aiming to contribute to groundbreaking innovations and
+          projects in the tech landscape.
+        </p>
+        </div>
+    </div>
+  );
+} 
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,80 @@
+import "../../style/contact.css"
+
+export default function Contact(){
+    return(
+        <div className="contact-box">
+        <div className="contact-content">
+          <h2>Get in Touch</h2>
+          <form className="contact-form">
+            <div className="form-group">
+              <label htmlFor="name">Name:</label>
+              <input type="text" id="name" name="name" required />
+            </div> 
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" name="email" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="message">Message:</label>
+              <textarea id="message" name="message" required></textarea>
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Send Message
+            </button>
+          </form>
+          <div className="contact-links">
+            <a
+              href="https://github.com/TayyabAli92"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="link-item"
+            >
+              <img
+            src="/github.png"
+            alt="github-pic"
+            width={300}
+            height={300}
+          />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/tayyab-ali-personal-86a3582b8/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="link-item"
+            >
+              <img
+            src="/linkedin.png"
+            alt="linkedin-pic"
+            width={300}
+            height={300}
+          />
+            </a>
+            <a
+              href="https://www.facebook.com/profile.php?id=6155675898995"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="link-item"
+            >
+              <img
+            src="/facebook.png"
+            alt="facebook-pic"
+            width={300}
+            height={300}
+          />
+            </a>
+            <a
+              href="mailto:tayyabali.personal@gmail.com"
+              className="link-item"
+            >
+              <img
+            src="/gmail.png"
+            alt="gmail-pic"
+            width={300}
+            height={300}
+          />
+            </a>
+          </div>
+        </div>
+      </div>
+    )
+}
