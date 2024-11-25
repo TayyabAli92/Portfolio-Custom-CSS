eddc7b20c248bd0493244e15edd719e3eb0ff8dc
Tayyab Ali <hameshak110@gmail.com>
2024-11-25 23:28:11 +0500

    update

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -9,7 +9,7 @@ export default function Project() {
       <div className="main-div">
         <div className="project-item">
           <Image
-            src="/countdown-timer.PNG"
+            src="/countdown timer.PNG"
             alt="Portfolio Item Image"
             width={300}
             height={250}
@@ -75,6 +75,54 @@ export default function Project() {
               target="_blank"
             >
               View List
+            </a>
+          </button>
+        </div>
+
+        <div className="project-item">
+          <Image
+            src="/digital-clock.PNG"
+            alt="clock Image"
+            width={300}
+            height={250}
+            className="portfolio-image"
+          />
+
+          <h3>Digital Clock</h3>
+          <p>
+            A task management application built with React and TypeScript,
+            designed for efficient organization and productivity.
+          </p>
+          <button type="button">
+            <a
+              href="https://next-js-digital-clock-two.vercel.app/"
+              target="_blank"
+            >
+              View Clock
+            </a>
+          </button>
+        </div>
+
+        <div className="project-item">
+          <Image
+            src="/calculator.PNG"
+            alt="clock Image"
+            width={300}
+            height={250}
+            className="portfolio-image"
+          />
+
+          <h3>Calculator</h3>
+          <p>
+            A task management application built with React and TypeScript,
+            designed for efficient organization and productivity.
+          </p>
+          <button type="button">
+            <a
+              href="https://next-js-simple-calculator.vercel.app/"
+              target="_blank"
+            >
+              View Calculator
             </a>
           </button>
         </div>
